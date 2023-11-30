e1d7f724f8fff61c5fa249b2d7ed0f2454da42ae
Lucas <74400487+lucasdns@users.noreply.github.com>
2023-11-30 15:25:06 +0100

    Add files via upload
    Étape 2: Installation de Docker sur la VM via Terraform 🐳
    Utilisez Terraform pour exécuter des scripts d'installation de Docker sur la VM.
    Assurez-vous que Docker est correctement installé et configuré via Terraform.

--- main.tf
+++ main.tf
@@ -2,12 +2,29 @@ terraform {
   required_providers {
     vultr = {
       source  = "vultr/vultr"
+      version = "2.17.1" 
     }
   }
 }
 
-resource "vultr_instance" "example" {
-  plan        = "vc2-1c-1gb" 
-  region      = "fra"      
-  os_id       = "387"         
+provider "vultr" {
+  api_key = "SILLVA2A6J3F6S4SKKSNXAPFNZFMWNFF2MRA"
 }
+
+resource "vultr_instance" "lucaaaaas" {
+  region  = "fra"
+  plan    = "vc2-1c-1gb"
+  os_id   = "352"
+
+
+ user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update
+              sudo apt-get install -y docker.io
+              sudo systemctl start docker
+              sudo systemctl enable docker
+              sudo docker run -d -p 80:80 --name mynginx nginx
+              EOF
+
+  }
+
