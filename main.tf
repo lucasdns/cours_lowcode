a6bc730d88888415bf6da02c7d5216f0dc055466
Lucas <74400487+lucasdns@users.noreply.github.com>
2023-11-30 15:32:52 +0100

    Add files via upload
    Étape 3: Clonage du dépôt et Lancement de l'Application Docker via Terraform 🚢
    Configurez Terraform pour construire et lancer le conteneur Docker, en s'assurant qu'il écoute sur le port 80.
    Assurez-vous que le site web est accessible via l'adresse IP de la VM.

--- main.tf
+++ main.tf
@@ -16,15 +16,21 @@ resource "vultr_instance" "lucaaaaas" {
   plan    = "vc2-1c-1gb"
   os_id   = "352"
 
-
- user_data = <<-EOF
+  user_data = <<-EOF
               #!/bin/bash
               sudo apt-get update
               sudo apt-get install -y docker.io
               sudo systemctl start docker
               sudo systemctl enable docker
-              sudo docker run -d -p 80:80 --name mynginx nginx
-              EOF
 
-  }
+              # Clonez le dépôt
+              git clone https://github.com/votre-utilisateur/cours_lowcode.git
+              cd cours_lowcode
 
+              # Construisez l'image Docker
+              sudo docker build -t votre-image-docker .
+
+              # Lancez l'application Docker
+              sudo docker run -d -p 80:80 --name mon-app-docker votre-image-docker
+            EOF
+}
