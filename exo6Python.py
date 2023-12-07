7ac278fbf53e1fe8fa7ba32c6324f4c4d35c5664
Lucas <74400487+lucasdns@users.noreply.github.com>
2023-12-07 14:20:44 +0100

    Delete exo6Python.py

--- exo6Python.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import sys
-
-nom_du_repo_souhaite = sys.argv[1]
-
-subprocess.run(["terraform", "init"])
-subprocess.run(["terraform", "apply", "-auto-approve", f"-var=nom_du_repo={nom_du_repo_souhaite}"])
